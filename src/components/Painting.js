32104f55adf876a155a3f222988e679ffaacf75b
Coffey May <jcoffeymay6@gmail.com>
2020-10-28 16:25:24 -0500

    styling card components

--- src/components/Painting.js
+++ src/components/Painting.js
@@ -5,15 +5,23 @@ import Logo from "../"
 function Painting({ title, medium, image }) {
     return (
         <div className="painting">
+
+
+
+            <div class="thumbnail">
+                <img src={image} />
+            </div>
             <h3>
                 {title}
             </h3>
-            <img src={image} />
-
-
-            <h5>
-                {medium}
-            </h5>
+            <footer>
+                <h5>
+                    {medium}
+                </h5>
+                <button>
+                    View Detail
+                </button>
+            </footer>
         </div>
     );
 }
