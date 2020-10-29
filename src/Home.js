8d8aa12b258905f1d3494905ad38f4f53cf3ca03
Coffey May <jcoffeymay6@gmail.com>
2020-10-29 01:46:53 -0500

    began parallax splashpage

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Parallax } from "react-parallax"
 
+const image1 = "/images/PChinatownbangkok.jpeg"
+
+
+const inline = {
+    color: '#fff',
+    left: '50%',
+    top: '50%',
+    position: 'absolute'
+}
 
 function Home() {
     return (
         <div className="Home">
-            <h1>Home</h1>
-        </div>
+            <Parallax id="bg1" bgImage={image1} strength={500}>
+                <div style={{ height: 500, backgroundColor: 'black', opacity: '0.5' }}>
+                    <div style={inline}>jdbfk</div>
+                </div>
+            </Parallax>
+            <div style={{ height: '100vh' }}></div>
+        </div >
     );
 }
 
