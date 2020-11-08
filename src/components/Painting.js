dd453fdd08ffb2fc27ba1ef210c491204928cf7e
Coffey May <jcoffeymay6@gmail.com>
2020-11-08 12:38:13 -0600

    began modal pop-up

--- src/components/Painting.js
+++ src/components/Painting.js
@@ -1,32 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import PaintingDetail from "./PaintingDetail"
 
 
 
 function Painting({ title, medium, image }) {
-    function detail() {
+    const [show, setShow] = useState(false)
 
-        console.log(title, medium)
-
-    }
+    const closeModalHandler = () => setShow(false)
 
     return (
-        <div className="painting">
-            <div className="thumbnail">
-                <img alt="oil painting" src={image} />
-            </div>
-            <h3>
-                {title}
-            </h3>
-            <h5>
-                {medium}
+        <>
+            <PaintingDetail title={title} show={show} closeModalHandler={closeModalHandler} />
+            <div className="painting">
+                <div className="thumbnail">
+                    <img alt="oil painting" src={image} />
+                </div>
+                <h3>
+                    {title}
+                </h3>
+                <h5>
+                    {medium}
 
-            </h5>
+                </h5>
 
-            <button onClick={detail} id="detail-btn">
-                View Detail
+                <button
+                    onClick={() => setShow(true)}
+                    id="detail-btn">
+                    View Detail
                 </button>
-        </div>
+
+            </div>
+
+        </>
     );
+
+
 }
 
 export default Painting;
--- src/components/PaintingDetail.js
+++ src/components/PaintingDetail.js
@@ -1,9 +1,25 @@
 import React from "react";
 
 
-function PaintingDetail() {
+function PaintingDetail({ show, closeModalHandler, title }) {
     return (
-        <div className="PaintingDetail">
+        <div className="modal-wrapper"
+            style={{
+                opacity: show ? "1" : "0"
+            }}>
+            <div className="modal-header">
+                <p>{title}</p>
+                <span onClick={closeModalHandler} className="close-modal-btn">x</span>
+
+            </div>
+            <div className="modal-content">
+                <div className="modal-body">
+                    <h4>Modal</h4>
+                    <p>jdbvskjhbvfjshdbfjlsdhbfjlhbdsfhjsdbfjlhsdbfjhbsdjhfbdsljfbjdfhbslj</p>
+                </div>
+                <div className="modal-footer"></div>
+                <button onClick={closeModalHandler} className="btn-cancel">Close</button>
+            </div>
 
         </div>
     );
