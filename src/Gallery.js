db5451c55b190f092b2e836bb9a45a7cb5cf1974
Coffey May <jcoffeymay6@gmail.com>
2020-11-18 18:36:47 -0600

    pre-build deploy testing

--- src/Gallery.js
+++ src/Gallery.js
@@ -7,21 +7,21 @@ import { FaCopyright } from 'react-icons/fa';
 
 
 const paintings = [
-    { id: 1, title: `Brooklyn Diner`, medium: `Oil on board`, image: `/images/PBrooklyndiner.jpeg` },
+    { id: 1, title: `Brooklyn Diner •NFS•`, medium: `Oil on board`, image: `/images/PBrooklyndiner.jpeg` },
     { id: 2, title: `Fruits`, medium: `Oil on paper`, image: `/images/PApple.jpeg` },
-    { id: 3, title: `The Pheasant`, medium: `Oil on canvas`, image: `/images/PAccelerator.jpeg` },
+    { id: 3, title: `The Pheasant •NFS•`, medium: `Oil on canvas`, image: `/images/PAccelerator.jpeg` },
     { id: 4, title: `Cheesburger`, medium: `Oil on paper`, image: `/images/PBurger.jpeg` },
     { id: 5, title: `Butterfly`, medium: `Oil on canvas`, image: `/images/PButterfly.jpeg` },
     { id: 6, title: `Cherries`, medium: `Oil on board`, image: `/images/PCherries.jpeg` },
     { id: 7, title: `Chinatown`, medium: `Acrylic on canvas`, image: `/images/PChinatownbangkok.jpeg` },
     { id: 8, title: `Consume`, medium: `Oil on canvas`, image: `/images/PProducts.jpeg` },
-    { id: 9, title: `Dandelion`, medium: `Oil on paper`, image: `/images/PDandelion.jpeg` },
+    { id: 9, title: `Dandelion •NFS•`, medium: `Oil on paper`, image: `/images/PDandelion.jpeg` },
     { id: 10, title: `Frog`, medium: `Oil on board`, image: `/images/PFrog.jpeg` },
 
     { id: 11, title: `Bird`, medium: `Oil on canvas`, image: `/images/PBird.jpeg` },
     { id: 12, title: `Cow`, medium: `Oil on canvas`, image: `/images/PCow.jpeg` },
     { id: 13, title: `The Dream`, medium: `Colored pencils on matboard`, image: `/images/PDream.jpeg` },
-    { id: 14, title: `Cheesburger`, medium: `Oil on paper`, image: `/images/PFly.jpeg` },
+    { id: 14, title: `The Fly •NFS•`, medium: `Oil on paper`, image: `/images/PFly.jpeg` },
     { id: 15, title: `Froggy`, medium: `Oil on paper`, image: `/images/PFroggy.jpeg` },
     { id: 16, title: `Harem of Saturn`, medium: `Oil on canvas`, image: `/images/PHos.jpeg` },
     { id: 17, title: `Gummy Worms`, medium: `Oil on board`, image: `/images/PGummyworms.jpeg` },
@@ -38,10 +38,10 @@ const paintings = [
     { id: 27, title: `Science`, medium: `Oil on canvas`, image: `/images/PPills.jpeg` },
     { id: 28, title: `River`, medium: `Oil on paper`, image: `/images/PLandscape.jpeg` },
     { id: 29, title: `Girl in tree`, medium: `Oil on canvas`, image: `/images/PRedhair.jpeg` },
-    { id: 30, title: `Spider`, medium: `Oil on canvas`, image: `/images/PSpider.jpeg` },
+    { id: 30, title: `Spider •NFS•`, medium: `Oil on canvas`, image: `/images/PSpider.jpeg` },
 
     { id: 31, title: `The Peacock`, medium: `Oil on canvas`, image: `/images/PPeacock.jpeg` },
-    { id: 32, title: `Teeth`, medium: `Oil on board`, image: `/images/PTeeth.jpeg` },
+    { id: 32, title: `Teeth •NFS•`, medium: `Oil on board`, image: `/images/PTeeth.jpeg` },
     { id: 33, title: `Tokyo Night Ride`, medium: `Oil on canvas`, image: `/images/PTokyo.jpeg` },
     { id: 34, title: `Girl and Tree`, medium: `Oil on paper`, image: `/images/PTreegirl.jpeg` },
     { id: 35, title: `Vaporwave`, medium: `Oil on paper`, image: `/images/PVapor.jpeg` },
@@ -55,14 +55,18 @@ const paintings = [
 
 function Gallery() {
     return (
-        <div className="Gallery">
+        <>
+            <div className="Gallery">
 
-            {
-                paintings.map((painting) => <Painting key={painting.id} title={painting.title} medium={painting.medium} image={painting.image} />)
-            }
-            <p style={{ marginTop: '5rem' }}><FaCopyright /> COPYRIGHT2020</p>
-        </div>
+                {
+                    paintings.map((painting) => <Painting key={painting.id} title={painting.title} medium={painting.medium} image={painting.image} />)
+                }
+                <p style={{ marginTop: '5rem' }}><FaCopyright /> COPYRIGHT2020</p>
 
+
+            </div>
+            <p>•NFS• <i>Not For Sale</i></p>
+        </>
     );
 }
 
