ae8462f61a0ba4767ad2bc23f5077ff9ef9a8f7b
Coffey May <jcoffeymay6@gmail.com>
2020-11-24 13:54:53 -0600

    minor changes to structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Nav from "./Nav";
 import Gallery from "./Gallery";
 import Contact from "./Contact";
 import Home from "./Home";
-//Main Component
+//Main
 function App() {
   return (
     <Router>
